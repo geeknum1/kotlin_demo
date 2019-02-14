e9f3c579ebc9dcb62d6b7d503da9849a4f7b0745
Mustang <569877490@qq.com>
2019-02-14 18:06:19 +0800

    修改support_version与complie_sdk_version一致

--- app/src/main/java/com/kotlin/demo/constant/Constant.kt
+++ app/src/main/java/com/kotlin/demo/constant/Constant.kt
@@ -8,6 +8,7 @@ object Constant {
      * baseUrl
      */
     val REQUEST_BASE_URL = "https://apicloud.mob.com/user/"
+    //val REQUEST_BASE_URL = "https://www.i3ni.cn/api/photo/page/1/"
 
     val KEY = "1be865c0e67e3"
 }
